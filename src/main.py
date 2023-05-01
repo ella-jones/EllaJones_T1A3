f8c1f9849a383d347bda0a85529dae133c807d0a
ella-jones <ellajonespriv@gmail.com>
2023-05-01 11:26:13 +0800

    Create menu_bar function

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
 print("Welcome to your Chores list!")
+
+def create_menu():
+    print("1. Enter 1 to add a chore to your list")
+    print("2. Enter 2 to remove a chore from your list")
+    print("3. Enter 3 to mark a chore as completed")
+    print("4. Enter 4 to view your chores list")
+    print("5. Enter 5 to view your chores for a specific day.")
+    print("6. Enter 6 to view your uncompleted chores")
+    selection = input("Enter your selection: ")
