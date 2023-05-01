c536dd5d5651f5013838aad096305f5b6da2bd04
ella-jones <ellajonespriv@gmail.com>
2023-05-01 11:31:40 +0800

    Add exit option to menu_bar function and return the users input (selection)

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 print("Welcome to your Chores list!")
 
-def create_menu():
+def menu_bar():
     print("1. Enter 1 to add a chore to your list")
     print("2. Enter 2 to remove a chore from your list")
     print("3. Enter 3 to mark a chore as completed")
     print("4. Enter 4 to view your chores list")
     print("5. Enter 5 to view your chores for a specific day.")
     print("6. Enter 6 to view your uncompleted chores")
+    print("7. Enter 7 to exit")
     selection = input("Enter your selection: ")
+    return selection
+
+user_selection = ""
+
