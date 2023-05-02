2cd80cd16bc2a23b4a5c18e6cb108f3a80e5be4e
ella-jones <ellajonespriv@gmail.com>
2023-05-02 21:57:56 +0800

    Add Pytest and create first test and a test_chores.csv file for the tests. & update requirements.txt to include pytest

---
+++ src/test_chores.py
@@ -0,0 +1,34 @@
+import pytest
+import csv
+from chore_functions import add_chore, remove_chore, view_chores
+
+test_file_name = "tests/test_chores.csv"
+
+def test_add(monkeypatch):
+    original_length = 0
+    with open(test_file_name) as f:
+        reader = csv.reader(f)
+        original_length = sum(1 for row in reader)
+    monkeypatch.setattr('builtins.input', lambda _: "Test Chore 1")
+    add_chore(test_file_name)
+    with open(test_file_name) as f:
+        reader = csv.reader(f)
+        new_length = sum(1 for row in reader)
+    print(original_length)
+    print(new_length)
+    assert new_length == original_length + 1
+
+def test_remove(monkeypatch):
+    original_length = 0
+    with open(test_file_name) as f:
+        reader = csv.reader(f)
+        original_length = sum(1 for row in reader)
+    monkeypatch.setattr('builtins.input', lambda _: "Test Chore 1")
+    remove_chore(test_file_name)
+    with open(test_file_name) as f:
+        reader = csv.reader(f)
+        new_length = sum(1 for row in reader)
+    print(original_length)
+    print(new_length)
+    assert new_length == original_length - 1
+
