27df258d228f9a6099364c29699cba253d9f78fa
ella-jones <ellajonespriv@gmail.com>
2023-05-03 13:42:56 +0800

    update second test

--- src/test_chores.py
+++ src/test_chores.py
@@ -2,9 +2,8 @@ import pytest
 import csv
 from chore_functions import add_chore, remove_chore, view_chores
 
-test_file_name = "tests/test_chores.csv"
-
 def test_add(monkeypatch):
+    test_file_name = "tests/test_chores.csv"
     original_length = 0
     with open(test_file_name) as f:
         reader = csv.reader(f)
@@ -19,11 +18,13 @@ def test_add(monkeypatch):
     assert new_length == original_length + 1
 
 def test_remove(monkeypatch):
+    test_file_name = "tests/test_remove.csv"
     original_length = 0
     with open(test_file_name) as f:
         reader = csv.reader(f)
         original_length = sum(1 for row in reader)
-    monkeypatch.setattr('builtins.input', lambda _: "Test Chore 1")
+    inputs = iter(['test', 'test2', 'test3'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     remove_chore(test_file_name)
     with open(test_file_name) as f:
         reader = csv.reader(f)
